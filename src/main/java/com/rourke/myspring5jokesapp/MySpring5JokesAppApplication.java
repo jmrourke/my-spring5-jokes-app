14050bff97be2e79730084fb3b1c09c8431f5674
James Rourke <jmrourke@yahoo.com>
2021-12-24 09:29:48 -0500

    Commit - initial project setup. closes #1

---
+++ src/main/java/com/rourke/myspring5jokesapp/MySpring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.rourke.myspring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpring5JokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rourke/myspring5jokesapp/MySpring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rourke.myspring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
